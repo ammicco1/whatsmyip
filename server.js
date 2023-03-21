903014a62f05579df14e6f87cfb7f9d21b57543c
ammicco <ngrecchi1@gmail.com>
2023-03-21 12:47:23 +0100

    add server code

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const server = express();
+
+const port = process.env.PORT || 3045;
+
+server.listen(port, function(){
+    console.debug(`whatsmyip server up on port ${port}`);
+});
+
+server.get("/", function(req, res){
+    let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    let ipv4 = ip.split(":")[ip.split(":").length - 1];
+    let ipv6 = ip.replace(ipv4, "");
+
+    let data = {
+        "ipv4": ipv4, "ipv6": ipv6
+    };
+
+    res.send(JSON.stringify(data));
+});
