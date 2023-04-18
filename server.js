08ba9e96bd35d40c51c28cc89b04bd5ac666c992
ammicco <ngrecchi1@gmail.com>
2023-04-18 18:26:56 +0200

    send json instead of string

--- server.js
+++ server.js
@@ -18,5 +18,32 @@ server.get("/", function(req, res){
         "ipv4": ipv4, "ipv6": ipv6
     };
 
-    res.send(JSON.stringify(data));
+    res.send(data);
+});
+
+server.get("/ipv4", function(req, res){
+    let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    let ipv4 = ip.split(":")[ip.split(":").length - 1];
+
+    console.debug(`req: ${ip}`);
+
+    let data = {
+        "ipv4": ipv4
+    };
+
+    res.send(data);
+});
+
+server.get("/ipv6", function(req, res){
+    let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    let ipv4 = ip.split(":")[ip.split(":").length - 1];
+    let ipv6 = ip.replace(ipv4, "");
+
+    console.debug(`req: ${ip}`);
+
+    let data = {
+        "ipv6": ipv6
+    };
+
+    res.send(data);
 });
